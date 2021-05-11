06e42d407b58e522101a8a819691c3d24e3c62ef
Saubhagya Chopra <75625298+saubhagya011@users.noreply.github.com>
2021-05-12 01:29:14 +0530

    Create Inverted Half Pyramid.cpp

---
+++ Inverted Half Pyramid.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, temp;
+    cout << "Enter the number of rows" << endl;
+    cin >> n;
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 0; j <=i; j++)
+        {
+            cout << "*" << " ";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
